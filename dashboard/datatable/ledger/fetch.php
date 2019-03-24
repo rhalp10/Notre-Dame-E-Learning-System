370c3681b8a3a0b0bb4c901577c5c6b1615d467c
Rhalp Darren R. Cabrera <rhalpdarrencabrera@gmail.com>
2019-03-25 00:12:05 +0800

    ledger

--- dashboard/datatable/ledger/fetch.php
+++ dashboard/datatable/ledger/fetch.php
@@ -33,7 +33,7 @@ foreach($result as $row)
 	$sub_array[] = $row["ledger_ID"];
 	$sub_array[] = $row["ledger_Name"];
 	$sub_array[] = $row["ledger_Amount"];
-	$sub_array[] = '<div class="dropdown"><button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Action<span class="caret"></span></button><ul class="dropdown-menu"><li><a href="#" id="'.$row["ledger_ID"].'" class="view">View Trial Balance</a><a href="#" id="'.$row["ledger_ID"].'" class="entry">Add Entry</a></li><li><a href="#" id="'.$row["ledger_ID"].'" class="update">Update</a></li><li><a href="#" id="'.$row["ledger_ID"].'" class="delete">Delete</a></li></ul></div>';
+	$sub_array[] = '<div class="dropdown"><button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Action<span class="caret"></span></button><ul class="dropdown-menu"><li><a href="#" id="'.$row["ledger_ID"].'" class="view_ledger">View Ledger</a><a href="#" id="'.$row["ledger_ID"].'" class="view_journal">View Journal</a></li><li><a href="#" id="'.$row["ledger_ID"].'" class="update">Update</a></li></ul></div>';
 	// $sub_array[] = '<button type="button" name="delete" id="'.$row["id"].'" class="btn btn-danger btn-xs delete">Delete</button>';
 	$data[] = $sub_array;
 }
--- dashboard/datatable/ledger/insert.php
+++ dashboard/datatable/ledger/insert.php
@@ -7,12 +7,15 @@ if(isset($_POST["operation"]))
 	if($_POST["operation"] == "Add")
 	{
 		$ledger_Name = $_POST["ledger_Name"];
-		$sql = "INSERT INTO `ledger` (`ledger_ID`, `ledger_Name`) VALUES (NULL, :ledger_Name);";
+		$ledger_Amount = $_POST["ledger_Amount"];
+
+		$sql = "INSERT INTO `ledger` (`ledger_ID`, `ledger_Name`,`ledger_Amount`) VALUES (NULL, :ledger_Name, :ledger_Amount);";
 		$statement = $conn->prepare($sql);
 		
 		$result = $statement->execute(
 			array(
-				':ledger_Name'			=>	$ledger_Name
+				':ledger_Name'			=>	$ledger_Name,
+				':ledger_Amount'		=>	$ledger_Amount
 			)
 		);
 
@@ -29,7 +32,7 @@ if(isset($_POST["operation"]))
 		
 		$ledger_Name = $_POST["ledger_Name"];
 		$ledger_Amount = $_POST["ledger_Amount"];
-		echo $sql ="UPDATE `ledger` SET `ledger_Name` = :ledger_Name,`ledger_Amount` = :ledger_Amount WHERE `ledger`.`ledger_ID` = :ledger_ID;";
+		 $sql ="UPDATE `ledger` SET `ledger_Name` = :ledger_Name,`ledger_Amount` = :ledger_Amount WHERE `ledger`.`ledger_ID` = :ledger_ID;";
 		
 		$statement = $conn->prepare($sql);
 		
